4b5899377e063e002a83aec178c80b0bf6ab1e40
Noah Huppert <contact@noahh.io>
2019-01-23 20:47:31 -0500

    server: added push model

---
+++ server/models/push.go
@@ -0,0 +1,16 @@
+package models
+
+// Push is a piece of media pushed by a device
+type Push struct {
+	// ID is a unique identifier
+	ID int64 `json:"id"`
+
+	// UserID is the ID of the user who sent the push
+	UserID int64 `json:"user_id"`
+
+	// FromDeviceID is the ID of the device who sent the push
+	FromDeviceID int64 `json:"from_device_id"`
+
+	// Text is the media sent in the push
+	Text string `json:"text"`
+}
