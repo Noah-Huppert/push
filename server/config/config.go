16bbe94e23e402036ec19593ba6678e12630f3a3
Noah Huppert <contact@noahh.io>
2019-01-24 08:28:21 -0500

    server: made config load from env vars

---
+++ server/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+// Config holds application configuration
+type Config struct {
+	// HTTPPort is the port the API will serve HTTP requests on
+	HTTPPort int `envconfig:"http_port"`
+}
+
+// NewConfigFromEnv load configuration options from environment variables
+func NewConfigFromEnv() (*Config, error) {
+	var cfg Config
+
+	if err := envconfig.Process("push_server", &cfg); err != nil {
+		return nil, fmt.Errorf("error loading configuration from "+
+			"environment variables: %s", err.Error())
+	}
+
+	return &cfg, nil
+}
