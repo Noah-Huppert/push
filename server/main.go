54aa9c29c103e9c09f600843ba33663bb2f6d5a0
Noah Huppert <contact@noahh.io>
2019-01-24 08:48:20 -0500

    server: made main prog wait for ctrl+c to exit

--- server/main.go
+++ server/main.go
@@ -34,7 +34,17 @@ func main() {
 		ctxCancel()
 	}()
 
+	// numDoneItems a count of the number of items which should send to the
+	// doneChan before the program should exit. Incremented throughout
+	// the main function
+	numDoneItems := 0
+
+	// doneChan receives any value when a part of the program is done
+	doneChan := make(chan int)
+
 	// {{{1 Setup HTTP server
+	numDoneItems++
+
 	// {{{2 Setup handlers
 	handler := mux.NewRouter()
 
@@ -45,23 +55,32 @@ func main() {
 
 	// {{{2 Start server
 	go func() {
-		err = server.ListenAndServe()
-		if err != nil {
+		if err = server.ListenAndServe(); err != nil &&
+			err != http.ErrServerClosed {
 			logger.Fatalf("error serving API: %s", err.Error())
 		}
-		logger.Debug("stopped API server")
+		logger.Info("stopped API server")
 	}()
 
 	// {{{2 Handle context close
 	go func() {
 		<-ctx.Done()
-		logger.Debug("stopping API server")
-		err := server.Shutdown(ctx)
-		if err != nil {
+		logger.Info("stopping API server")
+		if err := server.Shutdown(ctx); err != nil {
 			logger.Fatalf("error shutting down API server: %s",
 				err.Error())
 		}
+		doneChan <- 0
 	}()
 
-	logger.Debugf("started API server on port %d", cfg.HTTPPort)
+	logger.Infof("started API server on port %d", cfg.HTTPPort)
+
+	// {{{1 Wait for context to close
+	recDoneItems := 0
+	for recDoneItems < numDoneItems {
+		<-doneChan
+		recDoneItems++
+	}
+
+	logger.Info("program done")
 }
